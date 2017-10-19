3db5a5d878aaa2485c91b399603e83de7bb4af12
Sudeep Pillai <spillai@csail.mit.edu>
2017-10-19 15:17:42 -0400

    minor update to conda one time setup script

--- scripts/conda_onetime_setup.sh
+++ scripts/conda_onetime_setup.sh
@@ -4,7 +4,9 @@ conda create --name pybot --file conda_requirements.txt -y
 
 # Activate pybot and add activate/deactiavte scripts
 source activate pybot
-mkdir -p $CONDA_PREFIX/etc/conda
-cp config/activate $CONDA_PREFIX/etc/conda/
-cp config/deactivate $CONDA_PREFIX/etc/conda/
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+mkdir -p $CONDA_PREFIX/etc/conda/deactivate.d
+
+cp config/activate $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+cp config/deactivate $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
 
