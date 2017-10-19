755ec707bbe1476726782a94d09485e459c17e57
Sudeep Pillai <spillai@csail.mit.edu>
2017-10-19 15:08:39 -0400

    adding conda onetime setup script

--- scripts/conda_onetime_setup.sh
+++ scripts/conda_onetime_setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 conda config --add channels menpo
-conda create --name pybot --file conda_requirements.txt
+conda create --name pybot --file conda_requirements.txt -y
+
+# Activate pybot and add activate/deactiavte scripts
+source activate pybot
+mkdir -p $CONDA_PREFIX/etc/conda
+cp config/activate $CONDA_PREFIX/etc/conda/
+cp config/deactivate $CONDA_PREFIX/etc/conda/
 
